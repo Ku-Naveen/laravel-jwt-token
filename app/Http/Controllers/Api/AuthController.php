2d2e56aa4d4187fadf762cd81b28f01a37550ccc
Ku-Naveen <k.navinrajan23@gmail.com>
2023-02-14 10:28:23 +0530

    AuthController changes

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -28,24 +28,28 @@ class AuthController extends BaseController
      */
     public function login(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|email',
-            'password' => 'required|string|min:6',
-        ]);
-        if ($validator->fails()) {
-            return $this->sendError('Validation Error.', $validator->errors());
-        }
-        $credentials = $request->only('email', 'password');
-        $token = Auth::attempt($credentials);
-        if (!$token) {
-            return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
-        }
+        try {
+            $validator = Validator::make($request->all(), [
+                'email' => 'required|email',
+                'password' => 'required|string|min:6',
+            ]);
+            if ($validator->fails()) {
+                return $this->sendError('Validation Error.', $validator->errors());
+            }
+            $credentials = $request->only('email', 'password');
+            $token = Auth::attempt($credentials);
+            if (!$token) {
+                return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
+            }
 
-        $user = Auth::user();
-        $success['user'] =  $user;
-        $success['user']['token'] =  $token;
-        $success['user']['type'] =  'bearer';
-        return $this->sendResponse($success, 'User Login successfully.');
+            $user = Auth::user();
+            $success['user'] =  $user;
+            $success['user']['token'] =  $token;
+            $success['user']['type'] =  'bearer';
+            return $this->sendResponse($success, 'User Login successfully.');
+        } catch (\Exception $exception) {
+            return $this->sendError($exception->getMessage());
+        }
     }
 
     /**
@@ -55,26 +59,30 @@ class AuthController extends BaseController
      */
     public function register(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'firstname' => 'required|string|max:255',
-            'lastname' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
-            'password_confirmation' => 'required',
-            'dob' => 'required|date_format:d/m/Y'
-        ]);
-        if ($validator->fails()) {
-            return $this->sendError('Validation Error.', $validator->errors());
+        try {
+            $validator = Validator::make($request->all(), [
+                'firstname' => 'required|string|max:255',
+                'lastname' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users',
+                'password' => 'required|string|min:6|confirmed',
+                'password_confirmation' => 'required',
+                'dob' => 'required|date_format:d/m/Y'
+            ]);
+            if ($validator->fails()) {
+                return $this->sendError('Validation Error.', $validator->errors());
+            }
+            $user = User::create([
+                'firstname' => $request->firstname,
+                'lastname' => $request->lastname,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+                'dob' => $request->dob,
+            ]);
+            $success["user_id"] = $user->id;
+            return $this->sendResponse($success, 'User register successfully.');
+        } catch (\Exception $exception) {
+            return $this->sendError($exception->getMessage());
         }
-        $user = User::create([
-            'firstname' => $request->firstname,
-            'lastname' => $request->lastname,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-            'dob' => $request->dob,
-        ]);
-        $success["user_id"] = $user->id;
-        return $this->sendResponse($success, 'User register successfully.');
     }
 
     /**
@@ -84,9 +92,13 @@ class AuthController extends BaseController
      */
     public function logout()
     {
-        Auth::logout();
-        $success["user_id"] = '';
-        return $this->sendResponse($success, 'Successfully logged out');
+        try {
+            Auth::logout();
+            $success["user_id"] = '';
+            return $this->sendResponse($success, 'Successfully logged out');
+        } catch (\Exception $exception) {
+            return $this->sendError($exception->getMessage());
+        }
     }
 
      /**
@@ -96,9 +108,13 @@ class AuthController extends BaseController
      */
     public function refresh()
     {
-        $success['user'] =  Auth::user();
-        $success['user']['token'] =  Auth::refresh();
-        $success['user']['type'] =  'bearer';
-        return $this->sendResponse($success, 'Refresh Token');
+        try {
+            $success['user'] =  Auth::user();
+            $success['user']['token'] =  Auth::refresh();
+            $success['user']['type'] =  'bearer';
+            return $this->sendResponse($success, 'Refresh Token');
+        } catch (\Exception $exception) {
+            return $this->sendError($exception->getMessage());
+        }
     }
 }
